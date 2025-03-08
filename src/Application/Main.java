92116ea760fca323930ad91144ab47d3361f292b
Guillermo Sánchez Martínez <guigames2126@gmail.com>
2025-03-08 21:54:49 +0100

    user threads

--- src/Application/Main.java
+++ src/Application/Main.java
@@ -3,6 +3,7 @@ package Application;
 import Factory.ModelFactory;
 import Factory.ViewFactory;
 import Factory.ViewModelFactory;
+import Model.User;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
@@ -18,9 +19,28 @@ public class Main extends Application
     ModelFactory modelFactory = new ModelFactory();
     ViewModelFactory viewModelFactory = new ViewModelFactory(modelFactory);
     ViewFactory viewFactory = new ViewFactory(viewModelFactory, primaryStage);
-
+    
     viewFactory.getVinylListView();
     viewFactory.getCreateVinylView();
 
+    User user1 = new User("1");
+    User user2 = new User("2");
+    User user3 = new User("3");
+    User user4 = new User("4");
+
+    Thread thread1 = new Thread(user1);
+    Thread thread2 = new Thread(user2);
+    Thread thread3 = new Thread(user3);
+    Thread thread4 = new Thread(user4);
+
+    thread1.start();
+    thread2.start();
+    thread3.start();
+    thread4.start();
+
+    thread1.setDaemon(true);
+    thread2.setDaemon(true);
+    thread3.setDaemon(true);
+    thread4.setDaemon(true);
   }
 }
--- src/Model/User.java
+++ src/Model/User.java
@@ -1,12 +1,16 @@
 package Model;
 
-public class User
+import java.util.Random;
+
+public class User implements Runnable
 {
   private String name;
+  private Model model;
 
   public User(String name)
   {
     this.name = name;
+    this.model = new Model();
   }
 
   public String getName()
@@ -17,5 +21,32 @@ public class User
   public void setName(String name)
   {
     this.name = name;
+  }
+
+  @Override public void run()
+  {
+    while (true)
+    {
+      Random random = new Random();
+      int option = random.nextInt(2) + 1;
+      int vinylsSize = model.getVinylList().size();
+      int selectedVinyl = random.nextInt(vinylsSize);
+
+      switch (option)
+      {
+        case 1:
+          model.reserveVinyl(model.getVinylList().get(selectedVinyl),
+              this.getName());
+          break;
+        case 2:
+          model.borrowVinyl(model.getVinylList().get(selectedVinyl),
+              this.getName());
+          break;
+        case 3:
+          model.returnVinyl(model.getVinylList().get(selectedVinyl),
+              this.getName());
+          break;
+      }
+    }
   }
 }
